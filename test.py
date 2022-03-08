58cd9c1f44d5a4a1817ab95f6ef1cb8153548b1a
Ailin Dagnino <ailindagnino@gmail.com>
2022-03-08 20:15:29 -0300

    agregams test

---
+++ test.py
@@ -0,0 +1,16 @@
+## abro carpeta dnde quiero trabajar
+## dentro e terminar git, escribo code . y me abre un visual studio de esa carpeta
+## creo una catpea para trabajr cn git cn mkdir nombrecarpeta
+## abro con code nombrecarpeta
+
+## en visual studio, con bash escribo
+## git init
+## eso me inicializa el git, y me indica que estoy en master 
+## el archvo deeria verse en verde 
+## git status## para saber en que estado esta 
+
+## untracked files es porque no lo trbaajamos en git. 
+## en source control tmbpodemos ver estado 
+
+## para pasar de working directory a staging area, apretamos simbolito + y nos lo pasa a staged 
+## el - me lo vuelve a poner en cambios 
